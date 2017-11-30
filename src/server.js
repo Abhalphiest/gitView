92eb2d1909abe92fac4869bf63a63cef17f8b995
Margaret Dorsey <margaretdorsey94@gmail.com>
2017-11-30 00:31:32 -0500

    added more to server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
-const http = require('http');
-var port = process.env.PORT || process.env.NODE_PORT || 3000;
+"use strict"
 
-var githubAPIurl = "https://api.github.com";
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const GITHUB_API_URL = "https://api.github.com";
 
 http.createServer((request, response) => {
+
+  // handle errors
   request.on('error', (err) => {
     console.error(err);
     response.statusCode = 400;
@@ -12,23 +18,30 @@ http.createServer((request, response) => {
   response.on('error', (err) => {
     console.error(err);
   });
-  if (request.method === 'POST' && request.url === '/echo') {
-    request.pipe(response);
-  } 
-  else if(request.url === '/repo'){
-  	console.log('finding repo');
-  	response.statusCode = 200;
-  	response.write('finding repo');
-  	response.end();
+
+  let parsedUrl = url.parse(request.url);
+  let params = query.parse(parsedUrl.query);
+
+  //handle requests
+  switch(parsedUrl.pathname){
+	case '/repo': {
+	  	console.log('finding repo');
+	  	response.statusCode = 200;
+	  	response.write('finding repo');
+	  	response.end();
+	  	break;
+	}
+	case '/user':{
+	  	console.log('getting user');
+	  	requestUserData(params.username,response);
+	  	break;
+	}
+	default:{
+	  response.statusCode = 404;
+	  response.end();
+	}
   }
-  else if(request.url === '/user'){
-  	console.log('getting user');
-  	requestUserData("Abhalphiest",response);
-  }
-  else {
-    response.statusCode = 404;
-    response.end();
-  }
+  
 }).listen(port);
 
 console.log("Listening on localhost:"+ port);
@@ -40,13 +53,14 @@ function requestUserData(username, response){
 		path: '/users/'+username+'/repos'
 	};
 
-	var callback = function(response){
-		console.log(response);
+	var callback = function(xhrresponse){
+		console.log(xhrresponse);
+		response.statusCode = 200;
+		response.write(xhrresponse);
+  		response.end();
 	};
 
 	http.request(options,callback).end();
 
-	response.statusCode = 200;
-  	response.write('getting user');
-  	response.end();
+	response.write('getting user');
 }
